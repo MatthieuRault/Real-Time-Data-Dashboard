cddcd94c2a887e20157e71a4c090827c1bab3d87
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-12-18 17:01:08 +0100

    feat: add cors

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activer le CORS
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET',
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
