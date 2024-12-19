096583949f32d1d5cedcdebb2f65072be66e9be8
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-12-19 19:42:44 +0100

    feat: add cache

--- Back/src/modules/crypto/crypto.controller.ts
+++ Back/src/modules/crypto/crypto.controller.ts
@@ -4,14 +4,34 @@ import { CryptoService } from './crypto.service';
 @Controller('crypto')
 export class CryptoController {
   constructor(private cryptoService: CryptoService) {}
+  private cache = {
+    topCryptos: {
+      datas: [],
+      timestamp: null,
+    },
+  };
 
   @Get()
   async getTopCryptos(@Query('limit') limit: number) {
-    return this.cryptoService.getTopCryptos(limit);
+    if (this.cache.topCryptos.timestamp && Date.now() - this.cache.topCryptos.timestamp < 60000) {
+      return this.cache.topCryptos.datas;
+    }
+    const topCryptos = await this.cryptoService.getTopCryptos(limit);
+    this.cache.topCryptos = { datas: topCryptos, timestamp: Date.now() };
+    return topCryptos;
   }
 
   @Get(':id')
   async getCryptoById(@Param('id') id: string) {
-    return this.cryptoService.getCryptoById(id);
+    if (
+      this.cache[id] &&
+      this.cache[id].timestamp &&
+      Date.now() - this.cache[id].timestamp < 60000
+    ) {
+      return this.cache[id].datas;
+    }
+    const crypto = await this.cryptoService.getCryptoById(id);
+    this.cache[id] = { datas: crypto, timestamp: Date.now() };
+    return crypto;
   }
 }
