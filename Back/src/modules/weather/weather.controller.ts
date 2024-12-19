11897db6471c5ef0deb35e5ad3365bfdb2befb7c
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-12-19 20:03:26 +0100

    feat: add cache for weather

--- Back/src/modules/weather/weather.controller.ts
+++ Back/src/modules/weather/weather.controller.ts
@@ -4,9 +4,20 @@ import { WeatherService } from './weather.service';
 @Controller('weather') // Création de route /weather
 export class WeatherController {
   constructor(private weatherService: WeatherService) {} // Injection de WeatherService
+  private cache = {};
 
   @Get(':city') // Route /weather/:city
   async getWeatherByCity(@Param('city') city: string) {
-    return this.weatherService.getWeatherByCity(city);
+    city = city.toLowerCase();
+    if (
+      this.cache[city] &&
+      this.cache[city].timestamp &&
+      Date.now() - this.cache[city].timestamp < 60000
+    ) {
+      return this.cache[city].datas;
+    }
+    const weather = await this.weatherService.getWeatherByCity(city);
+    this.cache[city] = { datas: weather, timestamp: Date.now() };
+    return weather;
   }
 }
